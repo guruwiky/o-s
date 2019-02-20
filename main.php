c0926802c6083bdd73a0ea99d0cfc0c9e4a8a88a
guruwiky <ochiengwyclife6@gmail.com>
2019-02-20 10:41:08 +0300

    for hosting

---
+++ main.php
@@ -0,0 +1,29 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname="portfolio";
+$firstname=filter_input(_post,firstname);
+$lastname=filter_input(_post,lastname);
+$emailaddress=filter_input(input_post,emailaddress);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if (mysqli_connect_error()) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+$sql = "INSERT INTO clients (firstname, lastname, emailaddress)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
